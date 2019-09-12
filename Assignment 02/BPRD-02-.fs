8b2e1cda8c2d0b8688749c26634c7babb3fb6c7c
Xideta <mauh@itu.dk>
2019-09-12 15:37:49 +0200

    Set up an EntryPoint so it's easier to start with

--- Assignment 02/BPRD-02-.fs
+++ Assignment 02/BPRD-02-.fs
@@ -1,1 +1,52 @@
 ﻿
+
+open System
+open Parse
+open Absyn
+
+
+[<EntryPoint>]
+let main argv =
+    
+    try
+        printfn "%A" (fromString "1 + 2 * 3")
+    with
+        | :? System.Exception -> printfn "Error parsing input"
+    try
+        printfn "%A" (fromString "1 - 2 - 3")
+    with
+        | :? System.Exception -> printfn "Error parsing input"
+    try
+        printfn "%A" (fromString "1 + -2")
+    with
+        | :? System.Exception -> printfn "Error parsing input"
+    try
+        printfn "%A" (fromString "x++")
+    with
+        | :? System.Exception -> printfn "Error parsing input"
+    try
+        printfn "%A" (fromString "1 + 1.2")
+    with
+        | :? System.Exception -> printfn "Error parsing input"
+    try
+        printfn "%A" (fromString "1 + ")
+    with
+        | :? System.Exception -> printfn "Error parsing input"
+    try
+        printfn "%A" (fromString "let z = (17) in z + 2 * 3 end")
+    with
+        | :? System.Exception -> printfn "Error parsing input"
+    try
+        printfn "%A" (fromString "let z = 17) in z + 2 * 3 end")
+    with
+        | :? System.Exception -> printfn "Error parsing input"
+    try
+        printfn "%A" (fromString "let in = (17) in z + 2 * 3 end")
+    with
+        | :? System.Exception -> printfn "Error parsing input"
+    try
+        printfn "%A" (fromString "1 + let x=5 in let y=7+x in y+y end + x end")
+    with
+        | :? System.Exception -> printfn "Error parsing input"
+    
+    0
